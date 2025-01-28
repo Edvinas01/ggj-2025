7a73310cb6fed63b7a24c5f61ce78cc73450d1cd
edd <edvinas108@gmail.com>
2025-01-28 22:19:47 +0200

    Add heart tracking

---
+++ Assets/Scripts/Runtime/Components/Triggers/HealthTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using CHARK.GameManagement;
+using UABPetelnia.GGJ2025.Runtime.Actors;
+using UnityEngine;
+
+namespace UABPetelnia.GGJ2025.Runtime.Components.Triggers
+{
+    internal sealed class HealthTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> hearts;
+
+        private void OnEnable()
+        {
+            GameManager.AddListener<PlayerHealthChanged>(OnPlayerHealthChanged);
+        }
+
+        private void OnDisable()
+        {
+            GameManager.RemoveListener<PlayerHealthChanged>(OnPlayerHealthChanged);
+        }
+
+        private void OnPlayerHealthChanged(PlayerHealthChanged message)
+        {
+            var health = message.Player.Health;
+            for (var index = 0; index < hearts.Count; index++)
+            {
+                var heart = hearts[index];
+                heart.SetActive(index < health);
+            }
+        }
+    }
+}
